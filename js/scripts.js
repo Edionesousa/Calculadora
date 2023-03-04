652464e73fdc0e909de7c85baca38999d96a4496
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-03-04 14:17:01 -0300

    novo

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,14 @@
-const previousOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
+const previousOperationText = document.querySelector("#previous-operation")
+const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("#buttons-container button")
 
 class calculator{
-    constructor(previousOperationText, currentOperationText){
-        this.previousOperationText = previousOperationText
-        this.currentOperationText = currentOperationText
-        this.currentOperationText = ""
-    }
-    addDigit(digit){
-        
-            this.currentOperation = digit
-    }
+
 }
-const calc = new calculator(previousOperationText, currentOperationText);
 
-buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", (e) =>{
         const value = e.target.innerText;
-
-        if (+value >= 0 || value === "."){
-            calc.addDigit(value)
-        } else {
-            console.log("Op: " + value);
-        }
-    });
-});
+        console.log(value)
+    })
+})
