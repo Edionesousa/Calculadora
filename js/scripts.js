29a9708a40b54bd2c7b1b1582f170a127b4971f6
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-03-07 17:18:14 -0300

    novo

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,52 @@ class calculator{
         this.currentOperationText = currentOperationText
         this.currentOperation = ""
     }
+    processOperation(operation){
+        let operationValue
+        const previous = +this.previousOperationText.innerText
+        const current = +this.currentOperationText.innerText
 
+        switch(operation){
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+                return
+        }
+    }
     addDigit(digit){
+
+        if(digit === "." && this.currentOperationText.innerText.includes(".")) {
+            return
+        }
         this.currentOperation = digit
         this.updateScreen()
+
+        processOperation(operation)
+        
     }
         
-        updateScreen(){
-            this.currentOperationText.innerText += this.currentOperation
+        updateScreen(
+            operationValue = null,
+            operation = null,
+            current = null,
+            previous = null
+        ){
+            console.log(previousOperationText, currentOperationText)
+
+            this.currentOperationText.innerText += this.currentOperationText
+            this.currentOperation = ""
         }
 }
 const calc = new calculator(previousOperationText, currentOperationText)
 
-buttons.forEach((btn)=>{
-    btn.addEventListener("click", e =>{
+buttons.forEach((btn => {
+    btn.addEventListener("click", (e) =>{
         const value = e.target.innerText
         if( +value >= 0 || value === "."){
             calc.addDigit(value)
         }else{
-            console.log("op:" + value)
+            calc.processOperation(value)
         }
     })
 })
