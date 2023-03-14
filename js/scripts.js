8cc12f304c797147d49593fc6b8a955374cd56f3
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-03-14 16:04:24 -0300

    estudo

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,30 @@ const buttons = document.querySelectorAll("#buttons-container button");
 
 
 class calculator{
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+
+    };
+
+    addDigit(digit){
+
+    }
 
 };
+
+const calc = new calculator(previousOperationText, currentOperationText);
+
 
 buttons.forEach((btn) =>{
     btn.addEventListener("click", (e) =>{
         const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+            console.log(value);
+        }else{
+            console.log("Op:" + value);
+        }
     })
 })
-console.log(value);
