15ca1ee24b554da46dc983a9a140828c0338d677
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-12 12:35:18 -0300

    estudo js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-const previousOperationTest = document.querySelector("#previousOperation");
-const currentOperationText = document.querySelector("#currentOperation");
-const buttons = document.querySelectorAll("#buttons-container buttons")
-const previousOperation = "" ;
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("currentOperation");
+const buttons = document.querySelectorAll("buttons-container button");
 
 
 
