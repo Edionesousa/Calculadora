8fdb2558b800cf8cc310319501697dadc57f570d
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-12 12:10:05 -0300

    estudo js

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,7 @@
-
-
-const previousOperationText = document.querySelector("#previousOperation");
+const previousOperationTest = document.querySelector("#previousOperation");
 const currentOperationText = document.querySelector("#currentOperation");
-const buttons = document.querySelectorAll("#buttons-container button");
-
-class calculator{
-    constructor(previousOperationText, currentOperationText){
-        this.previousOperationText = previousOperationText
-        this.currentOperationText = currentOperationText
-        this.currentOpreation = "";
-    }
-    //add digit to calculator screen
-    addDigit(digit){
-        console.log(digit);
-
-    }
-    
-}
-
-const calc = new (previousOperationText, currentOperationText);
-
-
-buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-
-        const value = e.target.innerText;
-
-
-        if(+value >= 0 || value === "."){
-            calc.eddDigit(value)
-        }else{
-            console.log("op" + value)
-        }
-
-    });
-});
-
-
-
-
+const buttons = document.querySelectorAll("#buttons-container buttons")
+const previousOperation = "" ;
 
 
 
